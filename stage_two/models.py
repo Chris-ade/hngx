6f398588c945226da1d3a77060117da95f2d7d75
Chris Adebiyi <101924502+Chris-ade@users.noreply.github.com>
2023-09-10 21:42:47 +0100

    Update models.py

--- stage_two/models.py
+++ stage_two/models.py
@@ -3,6 +3,9 @@ class Person(db.Model):
     '''
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255))
-    gender = db.Column(db.String(6))
-    age = db.Column(db.Integer)
+    email = db.Column(db.String(255), nullable=False)
+    gender = db.Column(db.String(6), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    
+    def __repr__(self):
+        return f'<Person {self.name}>'
