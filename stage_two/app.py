e1183f85dc0fb10dc82bae0b01d4cedfcd1c31b0
Chris Adebiyi <101924502+Chris-ade@users.noreply.github.com>
2023-09-12 01:53:58 +0100

    Update app.py to fix an issue

--- stage_two/app.py
+++ stage_two/app.py
@@ -15,7 +15,7 @@ class Person(db.Model):
     ''' This is a model that initialize a table titled `person` with columns id, name, email, gender and age.
     '''
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False, unique=True)
+    name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     gender = db.Column(db.String(6), nullable=False)
     age = db.Column(db.Integer, nullable=False)
